bd9aa6235be540c4c8519deb750fb8f111351cd7
Ivancheo <99643630+Ivancheo@users.noreply.github.com>
2022-03-21 21:51:23 +0700

    Delete lab1/src/com/company directory

--- lab1/src/com/company/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Constants {
-    final static int INPUT_FILE = 0;
-    final static int OUTPUT_FILE = 1;
-    final static int ARGUMENTS_COUNT = 2;
-    final static int DIGITS_AFTER_DOT = 3;
-    final static double PERCENTS = 100.0;
-}
